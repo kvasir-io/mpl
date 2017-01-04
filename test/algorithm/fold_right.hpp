39c28db66a57488c7f70e05325b35746495bf09b
Odin Holmes <holmes.odin@gmail.com>
2017-01-04 17:11:57 +0100

    added skelleton structure

---
+++ test/algorithm/fold_right.hpp
@@ -0,0 +1,6 @@
+#pragma once
+/*==================================================================================================
+Copyright (c) 2016 Odin Holmes
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+=================================================================================================**/
