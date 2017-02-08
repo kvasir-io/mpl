9d478c7ee883266213457624dc2b81fbf899e0c1
chieltbest <chieltbest@gmail.com>
2017-02-08 17:27:08 +0100

    Change the license headers to the standard boost header

--- test/algorithm/all.hpp
+++ test/algorithm/all.hpp
@@ -1,6 +1,5 @@
+//          Copyright Odin Holmes 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 #pragma once
-/*==================================================================================================
-Copyright (c) 2016 Odin Holmes
-Distributed under the Boost Software License, Version 1.0.
-(See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-=================================================================================================**/
