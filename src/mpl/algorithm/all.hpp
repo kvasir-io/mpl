8c69598171dcc46b3769fd4fb467732d518d7b42
chieltbest <chieltbest@gmail.com>
2017-02-08 17:39:22 +0100

    Fix file naming discrepancy in the license header

--- src/mpl/algorithm/all.hpp
+++ src/mpl/algorithm/all.hpp
@@ -1,5 +1,5 @@
 //          Copyright Odin Holmes 2016.
 // Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
+//     (See accompanying file LICENSE.md or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 #pragma once
