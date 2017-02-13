e3ad325f32d5914171b0b52a60efac9aaf024c32
chieltbest <chieldouwes@gmail.com>
2017-02-13 18:07:09 +0100

    Add integral_constant type

---
+++ src/mpl/types/integral_constant.hpp
@@ -0,0 +1,24 @@
+//          Copyright Chiel Douwes 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+#pragma once
+
+namespace kvasir {
+	namespace mpl {
+		template <typename T, T val>
+		struct integral_constant {
+			constexpr operator T() {
+				return val;
+			}
+		};
+
+		template <bool val>
+		using bool_constant = integral_constant<bool, val>;
+
+		template <typename T>
+		constexpr auto make_integral(const T &&val) -> integral_constant<T, val> {
+			return {};
+		}
+	}
+}
