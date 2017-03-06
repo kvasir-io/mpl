bc7661be6b2cfd428a32c18a6867ed3a11bc8b1f
Odin Holmes <holmes.odin@gmail.com>
2017-03-06 19:52:32 +0100

    replaced partition with correct implementation
    the privious version of partition was actually another algorithm
    (probably split_at or something). we should name the algorithm that is
    equivalent to std::partition partition, we may need a new split_at.

--- src/kvasir/mpl/algorithm/partition.hpp
+++ src/kvasir/mpl/algorithm/partition.hpp
@@ -4,43 +4,17 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 #pragma once
 
-#include "../types/list.hpp"
-#include "../sequence/push_back.hpp"
-#include "../sequence/pop_front.hpp"
-#include "../sequence/create.hpp"
+#include "../algorithm/remove_if.hpp"
 
 namespace kvasir {
 	namespace mpl {
-		namespace impl {
-			namespace generic {
-				template <unsigned N>
-				struct partition_impl {
-					template <typename Result, typename List>
-					using f = typename partition_impl<N - 1>::template f<
-					        push_back_impl<typename pop_front_impl<List>::front, Result>,
-					        typename pop_front_impl<List>::rest>;
-				};
 
-				template <>
-				struct partition_impl<0> {
-					template <typename Result, typename List>
-					struct f {
-						using front = Result;
-						using rest  = List;
-					};
-				};
-			}
-
-			template <unsigned N, typename List>
-			struct partition_impl {
-				using f = typename generic::partition_impl<N>::template f<
-				        typename create_impl<List>::f, List>;
-			};
-		}
-
-		/// partition a list by a certain number of elements
-		/// returns a struct that is like the struct in pop_front
-		template <unsigned N, typename List>
-		using partition = typename impl::partition_impl<N, List>::f;
+		template <template <typename...> class Cond, typename List>
+		using partition = c::call<
+			list<  //using the forking version
+			c::listify, 
+			c::transform<c::join<c::listify>, c::list_wrap_if_not<Cond>>, //essentially a remove if
+			c::transform<c::join<c::listify>, c::list_wrap_if<Cond>>>, //essentially a filter
+			List>;
 	}
 }
