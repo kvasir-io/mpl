94f78c554fe44eb36b9a1f1ffeffb498ff4a81af
Emil Fresk <emil.fresk@gmail.com>
2017-03-06 22:37:26 +0100

    Added arithmetic operations

---
+++ src/kvasir/mpl/functions/arithmetic/min.hpp
@@ -0,0 +1,30 @@
+//          Copyright Chiel Douwes 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+#pragma once
+
+namespace kvasir {
+	namespace mpl {
+    namespace detail {
+
+        template<bool>
+        struct min_impl;
+
+        template<>
+        struct min_impl<true> {
+          template<typename A, typename B>
+          using f = A;
+        }
+
+        template<>
+        struct min_impl<false> {
+          template<typename A, typename B>
+          using f = B;
+        }
+    }
+
+    template<typename A, typename B>
+    using max = max_impl<A{} < B{}>::template f<A, B>;
+	}
+}
