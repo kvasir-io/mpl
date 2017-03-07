ad56e921371ce880a34b79a5127bd73ef8102354
Odin Holmes <holmes.odin@gmail.com>
2017-03-07 17:47:34 +0100

    implemented any in terms of all

--- src/kvasir/mpl/algorithm/any.hpp
+++ src/kvasir/mpl/algorithm/any.hpp
@@ -4,13 +4,20 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 #pragma once
 
-#include "contains.hpp"
+#include "all.hpp"
 
 namespace kvasir {
 	namespace mpl {
+		namespace c {
+			template<template<typename...> class F, typename TrueType = std::true_type, typename FalseType = std::false_type>
+			struct any {
+				template<typename...Ts>
+				using f = decltype(detail::all_tester::all_same<std::false_type, FalseType, TrueType>({ std::bool_constant<F<Ts>::value>{}... }));
+			};
+		}
 		/// filter elements from a list
 		/// takes a lambda that should return a type convertible to bool
 		template <template <typename...> class Cond, typename List>
-		using any = impl::contains_impl<Cond, List>;
+		using any = c::call<c::any<Cond>, List>;
 	}
 }
