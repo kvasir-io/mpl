2b0f05ac01c78120c64087b92de4f95ca96fc897
Odin Holmes <holmes.odin@gmail.com>
2017-03-08 15:54:16 +0100

    optimized count_if

--- src/kvasir/mpl/algorithm/count_if.hpp
+++ src/kvasir/mpl/algorithm/count_if.hpp
@@ -10,8 +10,15 @@
 namespace kvasir {
 	namespace mpl {
 		namespace c {
+			namespace detail {
+				template<template<typename...> class F>
+				struct list_wrap_void_if {
+					template<typename T>
+					using f = typename std::conditional<F<T>::value, list<void>, list<>>::type;
+				};
+			}
 			template<template <typename...> class Cond>
-			using count_if = filter<Cond, size>;
+			using count_if = transform<detail::list_wrap_void_if<Cond>, join<size>>;
 		}
 
 
