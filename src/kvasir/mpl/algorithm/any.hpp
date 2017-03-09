c4e978180e284e55578d50b119fc87c6975d6045
Odin Holmes <holmes.odin@gmail.com>
2017-03-09 18:37:53 +0100

    implemented any in terms of find_if

--- src/kvasir/mpl/algorithm/any.hpp
+++ src/kvasir/mpl/algorithm/any.hpp
@@ -4,20 +4,23 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 #pragma once
 
-#include "all.hpp"
+#include "find_if.hpp"
 
 namespace kvasir {
 	namespace mpl {
 		namespace c {
-			template<template<typename...> class F, typename TrueType = std::true_type, typename FalseType = std::false_type>
-			struct any {
-				template<typename...Ts>
-				using f = decltype(detail::all_tester::all_same<std::false_type, FalseType, TrueType>({ std::bool_constant<bool(F<Ts>::value)>{}... }));
-			};
+			namespace detail {
+				struct one_or_more {
+					template<typename...Ts>
+					using f = std::integral_constant<bool, (sizeof...(Ts) > 0)>;
+				};
+			}
+			template<typename F>
+			struct any : find_if<F, detail::one_or_more> {};
 		}
 		/// filter elements from a list
 		/// takes a lambda that should return a type convertible to bool
 		template <template <typename...> class Cond, typename List>
-		using any = c::call<c::any<Cond>, List>;
+		using any = c::call<c::any<bind<Cond>>, List>;
 	}
 }
