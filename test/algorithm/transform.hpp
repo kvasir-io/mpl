d556b0b2093355d54012326898ffa7f23048c545
Odin Holmes <holmes.odin@gmail.com>
2017-03-14 20:20:07 +0100

    added zip_with unit test

--- test/algorithm/transform.hpp
+++ test/algorithm/transform.hpp
@@ -9,6 +9,6 @@
 namespace {
 	using namespace kvasir;
 	static_assert(std::is_same<mpl::transform<mpl::list<void, char, short, int>, list>,
-		mpl::list<mpl::list<void>, mpl::list<char>, mpl::list<short>, mpl::list<int>>>{},
+		mpl::list<mpl::list<void>, mpl::list<char>, mpl::list<short>, mpl::list<int>>>::value,
 		"");
 }
