be4c8a2dbbe2daebe3b6a2e4775388fbbcb76ce5
Emil Fresk <emil.fresk@gmail.com>
2017-03-16 22:33:49 +0100

    Added logical operators
    Added comparison operators
    
    Updated mpl.hpp with the new functions

---
+++ src/kvasir/mpl/functions/comparison/greater_than.hpp
@@ -0,0 +1,14 @@
+//          Copyright Emil Fresk 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+#pragma once
+
+#include "../../types/integral_constant.hpp"
+
+namespace kvasir {
+	namespace mpl {
+		template <typename A, typename B>
+		using greater_than = integral_constant<decltype(A::value > B::value), (A::value > B::value)>;
+	}
+}
