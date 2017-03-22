55b2069575afbce51a3e674b9c9d63bf60f8630d
Odin Holmes <holmes.odin@gmail.com>
2017-03-22 23:27:55 +0100

    added fork_front

--- src/kvasir/mpl/functional/fork.hpp
+++ src/kvasir/mpl/functional/fork.hpp
@@ -11,8 +11,13 @@ namespace kvasir {
 			template <typename Combiner, typename... Cs>
 			struct fork {
 				template <typename... Ts>
-				using f = typename dcall<Combiner,sizeof...(Cs)>::template f<
-				        typename dcall<Cs,sizeof...(Ts)>::template f<Ts...>...>;
+				using f = typename dcall<Combiner, sizeof...(Cs)>::template f<
+					typename dcall<Cs, sizeof...(Ts)>::template f<Ts...>...>;
+			};
+			template <typename F, typename C>
+			struct fork_front {
+				template <typename... Ts>
+				using f = typename dcall<C, sizeof...(Ts)>::template f<typename dcall<F, sizeof...(Ts)>::template f<Ts...>, Ts...>;
 			};
 		}
 	}
