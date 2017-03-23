445c7137211df0b3e4cc3332b9ec33215df1fb39
chieltbest <chieldouwes@gmail.com>
2017-03-23 23:10:50 +0100

    Add test case for product and fix some issues with it

--- src/kvasir/mpl/algorithm/product.hpp
+++ src/kvasir/mpl/algorithm/product.hpp
@@ -4,19 +4,35 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 #pragma once
 
-#include "../functional/fork.hpp"
-#include "../sequence/push_back.hpp"
-#include "../sequence/push_front.hpp"
 #include "../algorithm/transform.hpp"
 #include "../functional/bind.hpp"
+#include "../functional/fork.hpp"
+#include "../sequence/join.hpp"
+#include "../sequence/push_back.hpp"
+#include "../sequence/push_front.hpp"
 
 namespace kvasir {
 	namespace mpl {
 		namespace c {
-			template<typename F = listify>
+			/*template <typename F = listify, typename C = listify>
 			struct product {
-				template<typename L0, typename L1, typename...Ls>
-				using f = call<call<transform<push_back<F, cfe<push_front, cfe<transform>>>, push_front<listify, cfe<fork>>>, L1>, L0>;
+			    template <typename L0, typename L1, typename... Ls>
+			    using f = call<call<transform<push_back<F, cfe<push_front, cfe<transform>>>,
+			                                  push_front<join<C>, cfe<fork>>>, L1>, L0>;
+			};*/
+
+			template <typename F = listify, typename C = listify>
+			struct product {
+				// push front the element from the first list, continue into F
+				template <typename T>
+				using transform_push = transform<push_front<T, F>>;
+
+				// call all the functions Ts with some input, joining the output
+				template <typename... Ts>
+				using fork_join = fork<join<C>, Ts...>;
+
+				template <typename L0, typename L1, typename... Ls>
+				using f = call<call<transform<cfe<transform_push>, cfe<fork_join>>, L1>, L0>;
 			};
 		}
 
