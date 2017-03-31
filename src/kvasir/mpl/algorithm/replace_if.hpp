477b1b9d50a51872efedd30ba19ab188f55b9a54
Odin Holmes <holmes.odin@gmail.com>
2017-03-31 14:48:28 +0200

    tuned replace_if

--- src/kvasir/mpl/algorithm/replace_if.hpp
+++ src/kvasir/mpl/algorithm/replace_if.hpp
@@ -18,15 +18,19 @@ namespace kvasir {
 				template <typename T>
 				using f = typename conditional<F::template f<T>::value>::template f<Input, T>;
 			};
-			template <template <typename...> class F, typename Input>
-			struct replace_if_pred<cfe<F, identity>, Input> {
-				template <typename T>
-				using f = typename conditional<F<T>::value>::template f<Input, T>;
-			};
 		}
 
+		//output is equivalent to the input parameter pack with every element 
+		//replaced with Input where the predicate F holds.
 		template <typename F, typename Input, typename C = listify>
 		using replace_if = transform<detail::replace_if_pred<F, Input>, C>;
+
+		//composition matching for common case
+		template <template <typename...> class F, typename Input, template <typename...> class C> 
+		struct transform<detail::replace_if_pred<cfe<F, identity>, Input>, cfe<C, identity>> {
+			template <typename... Ts>
+			using f = C<typename conditional<F<Ts>::value>::template f<Input, Ts>...>;
+		};
 
 		namespace eager {
 			template <typename List, typename Input, template <typename...> class Cond = identity>
