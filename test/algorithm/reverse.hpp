4b5608d797e64eb2d786bb202a572feb8c03fa2e
odinthenerd <odinthenerd@gmail.com>
2017-10-23 20:47:05 +0200

    updated docs

--- test/algorithm/reverse.hpp
+++
@@ -1,7 +0,0 @@
-//          Copyright Odin Holmes 2016.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE.md or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
-#pragma once
-
-#include <kvasir/mpl/algorithm/reverse.hpp>
--- test/functions/arithmetic/max.hpp
+++
@@ -1,7 +0,0 @@
-//          Copyright Chiel Douwes 2017.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE.md or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
-#pragma once
-
-#include <kvasir/mpl/functions/arithmetic/max.hpp>
