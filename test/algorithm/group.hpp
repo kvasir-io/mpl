d7d4acca4d179d7a30ba30e116e900d3d5af73d6
jonathan poelen <jonathan.poelen@gmail.com>
2017-10-25 02:24:56 +0200

    copyright

--- test/algorithm/group.hpp
+++ test/algorithm/group.hpp
@@ -1,4 +1,4 @@
-//          Copyright Odin Holmes 2016.
+//          Copyright Jonathan Poelen 2017.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE.md or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
