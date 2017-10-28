04f46bf1013300ce60c939061a9029f4295731c8
odinthenerd <odinthenerd@gmail.com>
2017-10-28 23:00:37 +0200

    changed all,any,count_if,filter and group to new test style

--- test/algorithm/group.hpp
+++ test/algorithm/group.hpp
@@ -11,16 +11,15 @@ namespace {
 	struct group_test {
 		group_test() {
 			using namespace kvasir::mpl;
-
-			eager::group<list<>>{}    = list<>{};
-			eager::group<list<int>>{} = list<list<int>>{};
-			eager::group<list<int, double>>{} = list<list<int>, list<double>>{};
-			eager::group<list<int, double, float>>{} = list<list<int>, list<double>, list<float>>{};
-			eager::group<list<int, int, double, double, double, int, int, int, float>>{} =
+			call<group<>>{}    = list<>{};
+			call<group<>,int>{} = list<list<int>>{};
+			call<group<>,int, double>{} = list<list<int>, list<double>>{};
+			call<group<>,int, double, float>{} = list<list<int>, list<double>, list<float>>{};
+			call<group<>,int, int, double, double, double, int, int, int, float>{} =
 			        list<list<int, int>, list<double, double, double>, list<int, int, int>,
 			             list<float>>{};
-			using seq           = eager::make_int_sequence<uint_<5>>;
-			eager::group<seq>{} = eager::transform<seq, list>{};
+			using seq           = call<make_int_sequence<>,uint_<5>>;
+			call<unpack<group<>>,seq>{} = call<unpack<transform<cfe<list>>>, seq>{};
 		}
 	};
 }
