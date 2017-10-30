a3ea2e814bfb6953676d8dd50af8f165e26f2b87
odinthenerd <odinthenerd@gmail.com>
2017-10-31 00:49:49 +0100

    added type wrapper

--- src/kvasir/mpl/algorithm/zip_with.hpp
+++ src/kvasir/mpl/algorithm/zip_with.hpp
@@ -16,6 +16,9 @@
 #include "../sequence/size.hpp"
 #include "../sequence/drop.hpp"
 
+#include "../algorithm/make_sequence.hpp"
+#include "../algorithm/rotate.hpp"
+
 namespace kvasir {
 	namespace mpl {
 		namespace detail {
@@ -91,6 +94,7 @@ namespace kvasir {
 			using f = typename detail::zip_with_unpack<F, C, Ts...>::f;
 		};
 		
+		/// \brief n-ary version of transform with all but one of the sequences as fixed parameters
 		template<typename F, typename...Ls>
 		struct zip_fixed{
 			private:
@@ -100,13 +104,13 @@ namespace kvasir {
 			template<typename...Ts>
 			using f = typename rotate<uint_<sizeof...(Ls)>,pop_front<zip_with<F,C>>>::template f<Ls...,list<Ts...>>;
 		};
-
+		/// \exclude
 		template<typename F, template<typename...> class L, typename...Ls, typename C>
 		struct zip_fixed<F,L<Ls...>,C>{
 			template<typename...Ts>
 			using f = typename dcall<C,sizeof...(Ts)>::template f<typename dcall<F, sizeof...(Ts)>::template f<Ls,Ts>...>;
 		};
-
+		/// \exclude
 		template<typename F, template<typename...> class L1, template<typename...> class L2,
 			typename...L1s, typename...L2s, typename C>
 		struct zip_fixed<F,L1<L1s...>,L2<L2s...>,C>{
@@ -114,6 +118,12 @@ namespace kvasir {
 			using f = typename dcall<C,sizeof...(Ts)>::template f<typename dcall<F, sizeof...(Ts)>::template f<L1s,L2s,Ts>...>;
 		};
 
+		/// \brief binary transform of an index sequence and the dynamic input sequence
+		template<typename F, typename C>
+		struct zip_with_index {
+			template<typename...Ts>
+			using f = typename zip_fixed<F,call<make_int_sequence<>,uint_<sizeof...(Ts)>>,C>::template f<Ts...>;
+		};
 		namespace eager {
 			template <template <typename...> class Func, typename... Lists>
 			using zip_with = call<zip_with<cfe<Func>>, Lists...>;
