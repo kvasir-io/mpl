f43145d1083c5070e6dbbcf36aa4c107c6b4f4cd
chieltbest <chieldouwes@gmail.com>
2017-12-13 06:50:46 +0100

    Add extreme algorithm

--- test/test.cpp
+++ test/test.cpp
@@ -7,6 +7,7 @@
 #include "algorithm/any.hpp"
 #include "algorithm/combinations.hpp"
 #include "algorithm/count_if.hpp"
+#include "algorithm/extreme.hpp"
 #include "algorithm/filter.hpp"
 #include "algorithm/find_if.hpp"
 #include "algorithm/flatten.hpp"
